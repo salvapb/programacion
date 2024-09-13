515cac651360bdfe4e9a31920bad417135afef08
salvapb <salvador.pagador@campus.xaloc.org>
2024-09-13 10:43:58 +0200

    Add files via upload

---
+++ programacion/python/Ejemplos/ejemplo.py
@@ -0,0 +1,16 @@
+var_entero=4
+var_dec=3.5
+var_text="hola"
+print(var_entero)
+print(var_dec)
+print(var_text)
+
+
+
+#maneras de visualizar la informacion de salida
+#manera 1 añadir texto ntre comillas, sepranado con comas la variable
+print("el resultado es: "var_entero , "manzanas")
+
+var_total=var_entero+var_dec
+#otra manera de print, con formato
+print(f"el resultado de sumar {var_entero} + {var_dec} + es: ",var_total)
