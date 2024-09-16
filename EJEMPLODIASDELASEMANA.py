c65880e17e02d3196c6ea29015cda07cc08b4f2b
salvapb <salvador.pagador@campus.xaloc.org>
2024-09-16 10:33:36 +0200

    Add files via upload

---
+++ EJEMPLODIASDELASEMANA.py
@@ -0,0 +1,5 @@
+var1=input("introduce el primer dia de la semana: ")
+var2=input("introduce el segundo dia de la semana: ")
+var3=input("introduce el tercer dia de la semana: ")
+
+print(var3,",",var2,",",var1)
