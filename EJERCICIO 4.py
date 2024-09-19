7ffed71ade95f5fb74585dd5c3fc63e022331949
salvapb <salvador.pagador@campus.xaloc.org>
2024-09-19 09:54:13 +0200

    Add files via upload

---
+++ EJERCICIO 4.py
@@ -0,0 +1,5 @@
+#4. A partir del código anterior, realiza una versión para números con decimales
+var1=float(input("introduce el primer número: "))
+var2=float(input("introduce el segundo número: "))
+total=var1+var2
+print("la suma de los dos numeroses: ",total)
