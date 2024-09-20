9121c4403ec248f48c5d5b686bfeab069a2cfcda
salvapb <salvador.pagador@campus.xaloc.org>
2024-09-20 10:45:46 +0200

    Add files via upload

---
+++ EJECICIO 9.py
@@ -0,0 +1,7 @@
+#9. programa que pida los segundos y muestre por pantalla y en la misma frase los minutos y las horas
+
+var1=int(input("introduce los segundos: "))
+minutos=var1/60
+horas=var1/3600
+print("los minutos son: ",minutos)
+print("las horas son: ",horas)
