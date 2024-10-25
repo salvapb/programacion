0a4ef629cbc77fc046dd08fe0e4e764362be5c25
salvapb <salvador.pagador@campus.xaloc.org>
2024-10-25 10:44:16 +0200

    Add files via upload

---
+++ ejercicio 30 condicionales inacabado.py
@@ -0,0 +1,14 @@
+#30. Realiza un programa que controle si la longitud de una frase introducida por teclado esigual, menor o mayor de 11 caracteres. Utiliza elif
+
+var1=input("introduce una frase: ")
+
+longitud=len(var1)
+
+if longitud>11:
+    print("la frase tiene uno o mas caracteres")
+
+elif longitud=11:
+    print("la frase tiene 11 caracteres")
+
+else:
+    print("la frase tiene menos de 11 caracteres")
