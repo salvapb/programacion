2e2a6c639853dd5446670553888a03ab3be9da99
salvapb <salvador.pagador@campus.xaloc.org>
2024-10-28 19:31:20 +0100

    Add files via upload

--- ejercicio 31 condicionales.py
+++ ejercicio 31 condicionales.py
@@ -1,10 +1,11 @@
 #31. Asigna a una variable de texto la siguiente frase: A quién madruga Dios ayuda. Comprueba si existen las siguientes palabras mostrando por pantalla la posición de su índice.
 
-var1= A quién madruga Dios ayuda 
+frase= "A quién madruga Dios ayuda"
 var2=input("introduce una palabra:")
 
-if var2 in var1:
-    print("la palabra esta en la frase y esta en el indice")
+indice=frase.find(var2)
+if var2 in frase:
+    print("la palabra esta en la frase y esta en el indice",indice)
     
 else:
     print("la palabra no esta en la frase") 
