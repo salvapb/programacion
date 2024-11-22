84d780e753434a8428e76465a29a14a6448c73a0
salvapb <salvador.pagador@campus.xaloc.org>
2024-11-22 10:16:06 +0100

    Update ejercicio 35.py

--- ejercicio 35.py
+++ ejercicio 35.py
@@ -1,3 +1,4 @@
+#35. Programa que al introducir un número por teclado permita mostrar ese número de veces tu nombre
 var1=input("introduce el nombre: ")
 var2=int(input("introduce el numero de veces que se repita el nombre: "))
 
