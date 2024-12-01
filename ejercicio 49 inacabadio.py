a679f5b9e481a66c8d01c3e792cf0b7b74c0739b
salvapb <salvador.pagador@campus.xaloc.org>
2024-12-01 11:14:51 +0100

    Delete ejercicio 49 inacabadio.py

--- ejercicio 49 inacabadio.py
+++
@@ -1,8 +0,0 @@
-posicion=1
-var=input("inttroduce una palabra: ")
-letra=input("introdyuce una letra: ")
-
-for recorrido in var:
-    if recorrido==letra:
-        print("la posicion de la letra", letra," es:", posicion)
-    posicion=posicion+1
