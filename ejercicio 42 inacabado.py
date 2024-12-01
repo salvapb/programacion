114d642134bb6e65c2d08d25d90342fadca48991
salvapb <salvador.pagador@campus.xaloc.org>
2024-12-01 11:15:07 +0100

    Delete ejercicio 42 inacabado.py

--- ejercicio 42 inacabado.py
+++
@@ -1,9 +0,0 @@
-#42. Imprima el siguiente patrón con el ciclo for. 
-var="*****"
-
-for contador in range(len(var)):
-    contador+1
-print(var[0:contador])
-
-for contador in range(len(var)):
-    print(var[contador:5])
