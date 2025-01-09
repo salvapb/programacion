2dada2ecb4047c7c1f1034aa40cc02fe32f9a91c
salvapb <salvador.pagador@campus.xaloc.org>
2025-01-09 09:52:41 +0100

    Add files via upload

---
+++ ejemplo1while.py
@@ -0,0 +1,11 @@
+contador=1
+contador_pares=0
+
+while contador<=20:
+    if contador%2==0:
+        contador_pares+=1
+       
+    contador+=1
+print("el numero de pares es: ",contador_pares)
+   
+ 
