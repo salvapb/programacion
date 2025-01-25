4e9a65c9cd1d8fd027aae89bc4b630cdcc547c4c
salvapb <salvador.pagador@campus.xaloc.org>
2025-01-25 10:49:19 +0100

    Add files via upload

--- ejercicio 61 while.py
+++ ejercicio 61 while.py
@@ -1,11 +1,13 @@
-#61. A partir del código anterior, haz que el programa finalice si el valor de la tabla de multiplicar es superior o igual a 40.
+#61. A partir del código anterior, haz que el programa finalice si el valor de la tabla de multiplicar essuperior o igual a 40.
+numero = int(input("Introduce el número que quieras multiplicar: "))
 
-numero=int(input("introduce el numero que quieras multiplicar: "))
-repeticiones=0
-while numero<40 and repeticiones!=10:
-    repeticiones+=1
-    numero=numero*repeticiones
-    if numero<40:
-        print(numero)
+repeticiones = 0
+
+while numero < 40 and repeticiones != 10:
+    repeticiones += 1
+    resultado = numero * repeticiones
+    if resultado < 40:
+        print(f"{numero} x {repeticiones} = {resultado}")
     else:
-        print("programa finalizado")
+        print("Programa finalizado")
+        break
