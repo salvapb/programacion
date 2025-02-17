e8a92300360a3fc2b852ea3133984b8c97c7199a
salvapb <salvador.pagador@campus.xaloc.org>
2025-02-17 10:36:06 +0100

    Add files via upload

--- ejercicio 69 listas.py
+++ ejercicio 69 listas.py
@@ -1,3 +1,4 @@
+#69. Realiza un programa que permita introducir una cantidad exacta de números, cada número se irá almacenando en una lista. El programa debe finalizar presentando por pantalla los números ordenados de menor a mayor
 cantidad = int(input("Introduce la cantidad de números que deseas ingresar: "))
 
 numeros = []
--- ejercicio 70 listas.py
+++ ejercicio 70 listas.py
@@ -1,3 +1,4 @@
+#70. Crea un programa que permita introducir x palabras en una lista llamada lista1. Una vez introducidas, crea una nueva lista, llamada lista2, exactamente igual a lista1. Se deben mostrar por pantalla el contenidos de lista1 en orden ascendente y lista2 en orden descendente. Respeta el formato de entrada y salida tal y como se muestra en el testeo.
 x = int(input("Introduce el número de palabras que deseas ingresar: "))
 
 lista1 = []
