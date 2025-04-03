e52520558e8e8f426aaf75d029720299d4ea8339
salvapb <salvador.pagador@campus.xaloc.org>
2025-04-03 09:50:20 +0200

    Delete EJEMPLODIASDELASEMANA.py

--- EJEMPLODIASDELASEMANA.py
+++
@@ -1,5 +0,0 @@
-var1=input("introduce el primer dia de la semana: ")
-var2=input("introduce el segundo dia de la semana: ")
-var3=input("introduce el tercer dia de la semana: ")
-
-print(var3,",",var2,",",var1)
