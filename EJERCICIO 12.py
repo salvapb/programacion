f1e34fae38bfa582eae4a9fd3040a0fc7226ceda
salvapb <salvador.pagador@campus.xaloc.org>
2025-04-03 09:51:18 +0200

    Delete EJERCICIO 12.py

--- EJERCICIO 12.py
+++
@@ -1,8 +0,0 @@
-var1=float(input("introduce el valor del lado del trapecio: "))
-var2=float(input("introduce el valor de la base menor del trapecio: "))
-var3=float(input("introduce el valor de la base mayor del trapecio: "))
-var4=float(input("introduce el valor de la altura del trapecio: "))
-perimetro=var1+var1+var2+var3
-print("el perimetro es: ",perimetro)
-area=(var2+var3)*var4/2
-print("el area es: ",area)
