e2233c855d3b66e6dff40e1ca0b1e1688b5d395c
salvapb <salvador.pagador@campus.xaloc.org>
2025-04-04 18:42:31 +0200

    Update EJERCICIO 14.py

--- Actividades Iniciales/EJERCICIO 14.py
+++ Actividades Iniciales/EJERCICIO 14.py
@@ -1,3 +1,5 @@
+#14. Realiza un programa que a partir de introducir el diámetro de un círculo calcule el área y perímetro. Importa la librería match y utiliza el valor PI para hacer el cálculo. Redondea el resultado a un decimal.
+
 import math
 var1=int(input("introduce el valor del diametro: "))
 perimetro=round(2*math.pi*(var1/2))
