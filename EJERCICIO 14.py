df2fc224949472db716ed1f3aad3921a849f4864
salvapb <salvador.pagador@campus.xaloc.org>
2025-04-04 10:04:19 +0200

    Delete EJERCICIO 14.py

--- EJERCICIO 14.py
+++
@@ -1,6 +0,0 @@
-import math
-var1=int(input("introduce el valor del diametro: "))
-perimetro=round(2*math.pi*(var1/2))
-print("el perimetro es: ",perimetro)
-area=round(math.pi*(var1/2)**2)
-print("el area es: ", area)
