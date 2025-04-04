789c1d5afc5a1ba371c1876d6d34f6f80581b4a6
salvapb <salvador.pagador@campus.xaloc.org>
2025-04-04 10:05:09 +0200

    Delete EJERCICIO 18.py

--- EJERCICIO 18.py
+++
@@ -1,6 +0,0 @@
-var1=int(input("introduce el numero de menores: "))
-var2=int(input("introduce el numero de adultos: "))
-precios_menores=12/2*var1
-print("el precio de los menores es: ",precios_menores)
-precio_adultos=12/1.1111*var2
-print("el precio de los adultos es: ",precio_adultos)
