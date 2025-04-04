9ed89801b53dab0d237646918b397ba2262361c0
salvapb <salvador.pagador@campus.xaloc.org>
2025-04-04 18:41:31 +0200

    Update EJERCICIO 12.py

--- Actividades Iniciales/EJERCICIO 12.py
+++ Actividades Iniciales/EJERCICIO 12.py
@@ -1,3 +1,4 @@
+#12. Realiza un programa que, introduciendo en los valores de lado, base menor, base mayor y altura de un trapecio isósceles, nos devuelva por pantalla en el área y el perímetro.
 var1=float(input("introduce el valor del lado del trapecio: "))
 var2=float(input("introduce el valor de la base menor del trapecio: "))
 var3=float(input("introduce el valor de la base mayor del trapecio: "))
