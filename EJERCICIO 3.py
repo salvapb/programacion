5381190b54965ab64098b4cf78e8372ca43d344e
salvapb <salvador.pagador@campus.xaloc.org>
2025-04-04 10:05:24 +0200

    Delete EJERCICIO 3.py

--- EJERCICIO 3.py
+++
@@ -1,5 +0,0 @@
-#3. Programa que pida dos números enteros y realice la suma correspondiente
-var1=int(input("introduce el primer numero: "))
-var2=int(input("introduce el segundo numero: "))
-total=var2+var1
-print("la suma de los dos numeros es: ",total)
