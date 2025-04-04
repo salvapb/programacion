7a2628895f1e9d38aaddeafbe1379d77ff113abd
salvapb <salvador.pagador@campus.xaloc.org>
2025-04-04 10:22:26 +0200

    Delete ejercicio 48.py

--- ejercicio 48.py
+++
@@ -1,8 +0,0 @@
-#48. Realiza un programa que introduzcas por teclado una palabra ‘secreta’, consigue la longitud de esa palabra para que sea ese el criterio que establezca el rango del bucle de manera que el usuario tenga x oportunidades para ver si letra introducida está en esa palabra
-palabra= input("Introduzca por teclado la palabra secreta: ")
-for recorrido in range(len(palabra)):
-    letra = input("Introduce una letra: ")
-    if letra in palabra:
-        print("La letra existe")
-    else:
-        print("La letra no existe")
