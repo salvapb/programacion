10fbef1d31b6146d956d2c680e4ca19e2f55cc85
salvapb <salvador.pagador@campus.xaloc.org>
2025-04-04 18:44:24 +0200

    Update EJERCICIO 18.py

--- Actividades Iniciales/EJERCICIO 18.py
+++ Actividades Iniciales/EJERCICIO 18.py
@@ -1,3 +1,5 @@
+#18. Cines Paradiso celebran su décimo aniversario y por ser un día especial realizanimportantes descuentos. A los adultos se les aplicará un 10% de descuento y a los menoresde 18 años un 50%. Si la entrada cuesta 12 euros, calcula el total a pagar introduciendo porteclado el número de menores y el número de adultos que asisten al cine.
+
 var1=int(input("introduce el numero de menores: "))
 var2=int(input("introduce el numero de adultos: "))
 precios_menores=12/2*var1
