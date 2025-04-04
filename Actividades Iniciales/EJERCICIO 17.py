ab53b455705481f6c581fdc277cdf2ae31c0857b
salvapb <salvador.pagador@campus.xaloc.org>
2025-04-04 18:43:58 +0200

    Update EJERCICIO 17.py

--- Actividades Iniciales/EJERCICIO 17.py
+++ Actividades Iniciales/EJERCICIO 17.py
@@ -1,3 +1,4 @@
+#17. Calcula el índice de masa corporal IMC de una persona, introduciendo por teclado elpeso (en kg) y dividiendo por la estatura (en metros y elevado al cuadrado). Si el resultadoes igual o superior a 25, debe aparecer un mensaje informando de sobrepeso.
 var1=float(input("introduzca el peso en kilogramos: "))
 var2=float(input("introduzca la estatura en metros: "))
 imc=var1/var2**2
