4b120ff2f06f203483276ee95067f2a6879644ad
salvapb <salvador.pagador@campus.xaloc.org>
2025-04-04 10:21:48 +0200

    Delete ejercicio 43.py

--- ejercicio 43.py
+++
@@ -1,6 +0,0 @@
-#43. Realiza un programa que recorra con un for una palabra introducida por teclado y se imprima por pantalla cada letra
-var_palabra = input("Introduzca una palabra: ")
-contador = 0
-for recorrido in var_palabra:
-    print("En la posición", contador,"está la", recorrido)
-    contador+=1
