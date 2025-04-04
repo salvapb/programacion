089b8b2f6c7a0419f346be7185819c9865da8ea5
salvapb <salvador.pagador@campus.xaloc.org>
2025-04-04 18:43:30 +0200

    Update EJERCICIO 16.py

--- Actividades Iniciales/EJERCICIO 16.py
+++ Actividades Iniciales/EJERCICIO 16.py
@@ -1,3 +1,5 @@
+#16. Utiliza el método sqrt de la librería math para calcular la raíz cuadrada de un número. Elresultado de la raíz cuadrada divídelo entre 2 de manera que se obtenga siempre unresultado entero. Haz que se muestre por pantalla los dos resultados de todo el proceso(raíz y división).
+
 import math
 var1=int(input("introduce un numero: "))
 print(math.sqrt(var1))
