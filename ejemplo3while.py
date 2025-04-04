3317e0177fe5f7674aa31b8c86d86bdc4bb6c240
salvapb <salvador.pagador@campus.xaloc.org>
2025-04-04 10:20:56 +0200

    Delete ejemplo3while.py

--- ejemplo3while.py
+++
@@ -1,7 +0,0 @@
-var_1=input("dime una contraseña de 8 caracteres: ")
-contraseña=len(var_1)
-while contraseña!=8:
-    print("no aceptado")
-    var_1=input("dime una contraseña de 8 caracteres: ")
-    contraseña=len(var_1)
-print("bien")
