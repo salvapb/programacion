846473c04ab52da2bf25dbd2d76bab4142af783d
salvapb <salvador.pagador@campus.xaloc.org>
2025-04-04 10:21:38 +0200

    Delete ejercicio 42.py

--- ejercicio 42.py
+++
@@ -1,9 +0,0 @@
-#42. Imprima el siguiente patrón con el ciclo for. 
-var="*****"
- 
-for contador in range(len(var)):
-    contador+=1
-    print(var[0:contador])
-
-for vuelta in range(len(var)):
-    print(var[vuelta:3])
