d8c52f9e8671274b99230a395df45ce07b43ca9d
salvapb <salvador.pagador@campus.xaloc.org>
2025-04-04 10:05:38 +0200

    Delete EJERCICIO 5.py

--- EJERCICIO 5.py
+++
@@ -1,8 +0,0 @@
-#5. Programa que pida cinco palabras y muestre una frase con las cinco. Modifica el código para que entre palabra y palabra haya una coma
-
-var1=input("introduce la primera palabra: ")
-var2=input("introduce la segunda palabra: ")
-var3=input("introduce la tercera palabra: ")
-var4=input("introduce la cuarta palabra: ")
-var5=input("introduce la quinta palabra: ")
-print(var1,",",var2,",",var3,",",var4,",",var5)
