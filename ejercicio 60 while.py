4345f6fae0d082652a10bd33189da4aeaf729d08
salvapb <salvador.pagador@campus.xaloc.org>
2025-04-04 10:30:44 +0200

    Delete ejercicio 60 while.py

--- ejercicio 60 while.py
+++
@@ -1,9 +0,0 @@
-#60. Diseña un programa que al introducir un número, realice su tabla de multiplicar del 1 al 10. Utiliza únicamente el while
-
-numero=int(input("introduce el numero que quieras multiplicar: "))
-numero_multi=0
-repeticiones=0
-while numero*10!=numero_multi and repeticiones!=11:
-    print(numero*repeticiones)
-
-    repeticiones+=1
