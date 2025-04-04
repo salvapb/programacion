0793309c46df17e96b2785e893410be8fa1b61d5
salvapb <salvador.pagador@campus.xaloc.org>
2025-04-04 19:07:36 +0200

    Add files via upload

---
+++ examen trimestral 2/ejercicio 1 examen.py
@@ -0,0 +1,11 @@
+numeros=input("introduce seis numeros:")
+
+lista=numeros
+print(lista)
+maximo=max(lista)
+print("el maximo es: ", maximo)
+minimo=min(lista)
+print("el minimo es:", minimo)
+suma_total=sum(numeros)
+promedio=suma_total/6
+print("el promedio es: ",promedio)
---
+++ examen trimestral 2/ejercicio 2 examen.py
@@ -0,0 +1,7 @@
+texto=input("introduce una cadena de texto: ")
+
+lista_palabras=[texto]
+
+palabra_buscada=input("introduce la palabra que deseas buscar: ")
+print(lista_palabras.count(palabra_buscada))
+
