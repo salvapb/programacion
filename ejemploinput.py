48fd0836d1c418a5e29e5741da38548e2120bf52
salvapb <salvador.pagador@campus.xaloc.org>
2025-04-04 10:20:33 +0200

    Delete ejemploinput.py

--- ejemploinput.py
+++
@@ -1,8 +0,0 @@
-var1=int(input("introduce el primer número: "))
-var2=int(input("introduce el segundo número: "))
-print("el primer numero introducido es:",var1)
-print("el segundo numero introducido es:",var2)
-
-total=var1+var2
-print("el resultado es: ",total)
-
