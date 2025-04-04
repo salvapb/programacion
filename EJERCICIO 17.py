8f224c54ac8524d5b4a850a5659f2c48c42cc06a
salvapb <salvador.pagador@campus.xaloc.org>
2025-04-04 10:05:02 +0200

    Delete EJERCICIO 17.py

--- EJERCICIO 17.py
+++
@@ -1,8 +0,0 @@
-var1=float(input("introduzca el peso en kilogramos: "))
-var2=float(input("introduzca la estatura en metros: "))
-imc=var1/var2**2
-print("tu indice de masa corporal es: ",imc)
-if imc>25:
-    print("hay sobrepeso")
-if imc==25:
-    print("hay sobrepeso")
