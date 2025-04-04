1d93d19e0376ad6c5384ebba73726440e1993e5b
salvapb <salvador.pagador@campus.xaloc.org>
2025-04-04 10:05:45 +0200

    Delete EJERCICIO 6.py

--- EJERCICIO 6.py
+++
@@ -1,7 +0,0 @@
-#6. A partir del programa 5. Haz que se muestre por pantalla también la frase en el orden inverso en que se han introducido las palabras.
-var1=input("introduce la primera palabra: ")
-var2=input("introduce la segunda palabra: ")
-var3=input("introduce la tercera palabra: ")
-var4=input("introduce la cuarta palabra: ")
-var5=input("introduce la quinta palabra: ")
-print(var5,",",var4,",",var3,",",var2,",",var1)
