241e1c9a09b7be5362d03e596f57ea0cdc2a251e
salvapb <salvador.pagador@campus.xaloc.org>
2025-04-04 10:22:33 +0200

    Delete ejercicio 49.py

--- ejercicio 49.py
+++
@@ -1,9 +0,0 @@
-#49. A partir del programa anterior, modifica el código para que al introducir la letra por teclado te indique en qué posición de la palabra se encuentra la letra.
-palabra = input("Introduce una palabra: ")
-
-for recorrido in range(len(palabra)):
-    letra = input("Introduce una letra: ")
-    for recorrido in range(len(palabra)):    
-        if palabra[recorrido]==letra:
-            print("La letra está en la posición",recorrido+1)
-      
